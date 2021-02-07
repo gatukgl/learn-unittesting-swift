5a1ae83531b9e2ea09432e0413759d1261a43c9f
Gatuk Chattanon <gatukgl@gmail.com>
2021-02-07 15:56:51 +0700

    Create user model

---
+++ LearnUnittests/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  LearnUnittests
+//
+//  Created by Gatuk Chattanon on 7/2/21.
+//
+
+import Foundation
+
+struct UserModel {
+    let firstname: String
+    let lastname: String
+    let email: String
+    let password: String
+    let confirmedPassword: String
+}
